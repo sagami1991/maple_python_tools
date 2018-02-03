45cc5bfe2fd86bd085971d2008f7a977353328b5
socket1016 <socket1016@users.noreply.github.com>
2018-02-03 13:57:16 +0900

    no message

--- gui.py
+++ gui.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python
 import sys
 from PyQt5.QtWidgets import *
+from PyQt5 import QtCore
 import time
 import game_controller
 import cv2
+import win32con
 
 class MainWindow(QFrame):
     def __init__(self) -> None:
         super(MainWindow, self).__init__()
-        self.setFixedSize(300, 200)
+        self.setFixedSize(200, 50)
         self.setWindowTitle("Maple auto HS 2pc")
-        button = QPushButton("仮想PC向け - 開始", self)
-        button.clicked.connect(MainWindow.start_watch_for_virtual)
+        self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
+        button = QPushButton("シグナスへ行く", self)
+        button.clicked.connect(MainWindow.go_to_cygnus)
         vbox = QVBoxLayout()
         hbox = QHBoxLayout()
         hbox.addWidget(button)
@@ -19,6 +22,44 @@ class MainWindow(QFrame):
         vbox.addLayout(hbox)
         vbox.addStretch(1)
         self.setLayout(vbox)
+
+    @staticmethod
+    def go_to_cygnus():
+        game = game_controller.GameController()
+        # イージーシグナスへ移動
+        game.active_game_window()
+        time.sleep(0.1)
+        game.send_key('T')
+        time.sleep(0.3)
+        img = game.take_png_screenshot()
+        center_point = game.template_match("Screenshot_152.png", img)
+        game.send_click(center_point)
+        time.sleep(0.1)
+        game.send_click(center_point)
+        time.sleep(0.1)
+        game.send_click(center_point)
+        time.sleep(0.1)
+        game.send_click([center_point[0] - 185, center_point[1]])
+        time.sleep(0.1)
+        game.send_click([center_point[0] - 25, center_point[1] + 265])
+        time.sleep(3)
+
+        # グループ申請
+        game.send_key('7')
+        time.sleep(0.1)
+        group_ss = game.take_png_screenshot()
+        find_group_point = game.template_match("Screenshot_157.png", group_ss)
+        if find_group_point is None:
+            return
+        game.send_click(find_group_point)
+        time.sleep(0.2)
+        group_list_ss = game.take_png_screenshot()
+        my_group_point = game.template_match("Screenshot_159.png", group_list_ss)
+        if my_group_point is None:
+            return
+        game.send_click([my_group_point[0]+160, my_group_point[1]])
+        time.sleep(0.3)
+        game.send_key(win32con.VK_RETURN)
 
     @staticmethod
     def start_watch_for_virtual():
@@ -43,7 +84,7 @@ class MainWindow(QFrame):
                 cv2.waitKey()
                 continue
             game.send_click(point, True)
-            time.sleep(0.1)
+            time.sleep(0.3)
 
             screen = game.take_png_screenshot()
             point = game.template_match("group_expulsion.png", screen)
@@ -57,13 +98,12 @@ class MainWindow(QFrame):
             time.sleep(5)
 
 
-
-
-
-
 if __name__ == '__main__':
-    MainWindow.start_watch_for_virtual()
-    # app = QApplication(sys.argv)
-    # window = MainWindow()
-    # window.show()
-    # sys.exit(app.exec_())
+    if sys.argv[1] == 'main':
+        app = QApplication(sys.argv)
+        window = MainWindow()
+        window.show()
+        sys.exit(app.exec_())
+    else:
+        MainWindow.start_watch_for_virtual()
+
